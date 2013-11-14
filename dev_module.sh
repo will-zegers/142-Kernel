1696b02a94e0e12cf57e7695255f40dcc13930de
Will Zegers <will.zegers@gmail.com>
2013-11-13 23:01:22 -0800

    Easy start shell command

--- dev_module.sh
+++ dev_module.sh
@@ -9,5 +9,6 @@ rmmod $modName 2> /dev/null
 wait
 insmod $modName.ko 
 echo "[+] Module $modName.ko inserted to the kernel"
+cat /proc/modules|grep hermes
 mknod $modName c 100 0 2>/dev/null
 echo "[+] Device $modName created"
